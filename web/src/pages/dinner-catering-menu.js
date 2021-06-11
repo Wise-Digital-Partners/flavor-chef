1c06be772ccb51f425a0b677531ffb50a11f3f18
Hamlet <55106549+hamletavetisian@users.noreply.github.com>
2021-06-11 00:16:30 -0700

    Create dinner-catering-menu.js

---
+++ web/src/pages/dinner-catering-menu.js
@@ -0,0 +1,408 @@
+import React from "react";
+import { graphql } from "gatsby";
+import { getImage } from "gatsby-plugin-image";
+
+import Layout from "../components/Layout";
+import SearchEngineOptimization from "../components/SEO";
+import HeroFullWidth from "../components/Hero/HeroFullWidth";
+import Divider from "../components/Repeating/Divider";
+import CallToAction from "../components/Repeating/CTA";
+
+const Page = ({ data }) => {
+  const heroFullWidthImages = [
+    getImage(data.heroDesktop.childImageSharp.gatsbyImageData),
+    {
+      ...getImage(data.heroMobile.childImageSharp.gatsbyImageData),
+      media: `(max-width: 767px)`,
+    },
+  ];
+
+  const appetizers = [
+    {
+      name: "Lollipop Lamb Chop",
+      description: "seared lamb chop, rosemary, garlic, dill-mustard Shrimp ",
+    },
+    {
+      name: "Lime Ceviche",
+      description:
+        "shrimp, cherry tomato, cucumber, avocado, red onion, micro cilantro, tostada",
+    },
+    {
+      name: "Ricotta & Tomato Jam Crostini",
+      description: "grilled baguette, tomato jam, whipped ricotta",
+    },
+    {
+      name: "Cantaloupe & Prosciutto Skewer",
+      description: "cantaloupe, prosciutto, reduced balsamic, mint",
+    },
+    {
+      name: "Cured Salmon Cucumber Roll Up",
+      description: "cured salmon, lemon dill cream cheese, cucumber",
+    },
+    {
+      name: "Duck Con fit Crostini",
+      description:
+        "toasted baguette, blackberry jam, duck confit, pickled kumquat",
+    },
+  ];
+
+  const salad = [
+    {
+      name: "Butter Lettuce Caesar",
+      description: "butter lettuce, crouton, blistered tomato, shaved parmesan",
+    },
+    {
+      name: "Watermelon Strawberry Salad",
+      description:
+        "arugula, watermelon, cucumber, feta, micro mint, lemon EVOO",
+    },
+    {
+      name: "Beets & Burrata ",
+      description:
+        " roasted red beets, strawberry, burrata, greens, ginger balsamic",
+    },
+  ];
+
+  const main = [
+    {
+      name: "Roasted Herb Chicken",
+      description:
+        "Jidori airline breast, herbs, garlic, grilled peach & Hatch green chili glaze",
+    },
+    {
+      name: "Braised Short Rib",
+      description: "slow braised beef, red wine demi, fried leeks",
+    },
+    {
+      name: "Citrus Seabass",
+      description: "local seabass, fresh citrus, apricot beurre blanc",
+    },
+    {
+      name: "Lobster Tail",
+      description: "butterflied lobster tail, lemon, butter, white wine",
+    },
+    {
+      name: "Pepper Crusted Filet Mignon",
+      description: "seared filet mignon, wild mushroom demi glace",
+    },
+    {
+      name: "Stuffed Portobella Mushroom",
+      description:
+        "marinated portobella, creamy spinach & artichoke heart, parmesan Reggiano",
+    },
+  ];
+
+  const starch = [
+    {
+      name: "Roasted Fingerling Potatoes",
+      description: "roasted fingerling potatoes, Calabrian chili oil",
+    },
+    {
+      name: "Summer Corn Risotto",
+      description:
+        "summer corn, saffron, arborio, white wine, parmesan Reggiano",
+    },
+    {
+      name: "Mashed Potatoes",
+      description: "yukon gold, roasted garlic, cream, butter",
+    },
+  ];
+
+  const vegetable = [
+    {
+      name: "Sauteed Broccolini",
+      description: "broccolini, garlic, butter",
+    },
+    {
+      name: "Grilled Eightball Squash",
+      description: "squash, garlic, sea salt",
+    },
+    {
+      name: "Petit Heirloom Carrots",
+      description: "heirloom carrot, fresh herbs, butter",
+    },
+  ];
+
+  const dessert = [
+    {
+      name: "Chocolate Lava Cake",
+      description: "chocolate cake, molten center, vanilla ice cream",
+    },
+    {
+      name: "Summer Berry Crème Brulee",
+      description: "fresh berries, sweet cream, caramelized sugar",
+    },
+    {
+      name: "Cherry Galette ",
+      description: "cherry compote, pie crust, vanilla ice cream",
+    },
+  ];
+
+  return (
+    <Layout
+      headerStyle="overlap"
+      headerLinkColor="white"
+      headerHasBorder={true}
+    >
+      <SearchEngineOptimization
+        title="Dinner Catering Menu | Flavor Chef Catering"
+        description="Transform any summer bash into a flavorful event with fresh dishes from Flavor Chef Catering. Our dinner catering menu has all the great eats!"
+        // openGraphImage={data.openGraphImage.publicURL}
+        // twitterOpenGraphImage={data.twitterOpenGraphImage.publicURL}
+      />
+
+      <HeroFullWidth
+        backgroundImages={heroFullWidthImages}
+        padding="pt-36 md:pt-64 pb-36 md:pb-64"
+        textAlignment="text-center"
+        textMaxWidth=""
+      >
+        <div className="text-white font-display text-xl tracking-wider leading-5 mb-4">
+          Menu
+        </div>
+        <h1 className="text-white mb-0">Summer Dinner Party</h1>
+      </HeroFullWidth>
+
+      <div className="pt-14 md:pt-18">
+        <section id="appetizers" className="mb-12 md:mb-16">
+          <div className="container">
+            <header className="mb-14 md:mb-12">
+              <h2>Appetizers</h2>
+            </header>
+
+            <div className="font-body text-sm md:text-base text-gray-600 text-opacity-40 tracking-wider uppercase border-b border-gray-400 border-opacity-30 pb-4 mb-8 md:mb-12">
+              <b>Appetizers</b> / Choose 3
+            </div>
+            <div className="grid md:grid-cols-2 md:gap-x-10 lg:gap-x-20 mb-10">
+              <div>
+                {appetizers.slice(0, 3).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div>
+                {appetizers.slice(3).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="salad" className="mb-12 md:mb-16">
+          <div className="container">
+            <header className="mb-14 md:mb-12">
+              <h2>Salad</h2>
+            </header>
+
+            <div className="font-body text-sm md:text-base text-gray-600 text-opacity-40 tracking-wider uppercase border-b border-gray-400 border-opacity-30 pb-4 mb-8 md:mb-12">
+              <b>Salad</b> / Choose 1
+            </div>
+            <div className="grid md:grid-cols-2 md:gap-x-10 lg:gap-x-20 mb-10">
+              <div>
+                {salad.slice(0, 2).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div>
+                {salad.slice(2).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="main" className="mb-12 md:mb-16">
+          <div className="container">
+            <header className="mb-14 md:mb-12">
+              <h2>Main</h2>
+            </header>
+
+            <div className="font-body text-sm md:text-base text-gray-600 text-opacity-40 tracking-wider uppercase border-b border-gray-400 border-opacity-30 pb-4 mb-8 md:mb-12">
+              <b>Main</b> / Choose 1 or 2
+            </div>
+            <div className="grid md:grid-cols-2 md:gap-x-10 lg:gap-x-20 mb-10">
+              <div>
+                {main.slice(0, 3).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div>
+                {main.slice(3).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="startch-vegetable" className="mb-12 md:mb-16">
+          <div className="container">
+            <header className="mb-14 md:mb-12">
+              <h2>Startch & Vegetable</h2>
+            </header>
+
+            <div className="grid md:grid-cols-2 gap-y-6 md:gap-x-10 lg:gap-x-20 mb-10">
+              <div>
+                <div className="font-body text-sm md:text-base text-gray-600 text-opacity-40 tracking-wider uppercase border-b border-gray-400 border-opacity-30 pb-4 mb-8 md:mb-12">
+                  <b>Startch</b> / Choose 1
+                </div>
+                {starch.map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div>
+                <div className="font-body text-sm md:text-base text-gray-600 text-opacity-40 tracking-wider uppercase border-b border-gray-400 border-opacity-30 pb-4 mb-8 md:mb-12">
+                  <b>Vegetable</b> / Choose 1
+                </div>
+                {vegetable.map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </section>
+
+        <section id="dessert" className="mb-12 md:mb-16">
+          <div className="container">
+            <header className="mb-14 md:mb-12">
+              <h2>Dessert</h2>
+            </header>
+
+            <div className="font-body text-sm md:text-base text-gray-600 text-opacity-40 tracking-wider uppercase border-b border-gray-400 border-opacity-30 pb-4 mb-8 md:mb-12">
+              <b>Dessert</b> / Choose 1
+            </div>
+            <div className="grid md:grid-cols-2 md:gap-x-10 lg:gap-x-20 mb-10">
+              <div>
+                {dessert.slice(0, 2).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div>
+                {dessert.slice(2).map((item, i) => {
+                  return (
+                    <div key={i} className="mb-8 md:mb-10">
+                      <p className="font-display text-xl text-gray-600 tracking-wider leading-6 mb-2">
+                        {item.name}
+                      </p>
+                      <p className="mb-0">{item.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <Divider />
+
+      <CallToAction headingLevel="h2" />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    openGraphImage: file(
+      relativePath: { eq: "open-graph/facebook/Homepage_FB.jpg" }
+    ) {
+      publicURL
+    }
+    twitterOpenGraphImage: file(
+      relativePath: { eq: "open-graph/twitter/Homepage_TW.jpg" }
+    ) {
+      publicURL
+    }
+    heroDesktop: file(
+      relativePath: {
+        eq: "menus/Summer Dinner Party Menu/1.0 Summer Dinner Party Hero Desktop.jpg"
+      }
+    ) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, quality: 100)
+      }
+    }
+    heroMobile: file(
+      relativePath: {
+        eq: "menus/Summer Dinner Party Menu/1.0 Summer Dinner Party Hero Mobile.jpg"
+      }
+    ) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, quality: 100)
+      }
+    }
+    menuIcon: file(relativePath: { eq: "global/menu.svg" }) {
+      publicURL
+    }
+    addOnIcon: file(relativePath: { eq: "menus/Add-on Icon.svg" }) {
+      publicURL
+    }
+  }
+`;
+export default Page;
